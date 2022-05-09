51ee16a48360ddb73f234fe095b69c4e3a04eb3b
1SNNS1 <72723468+1SNNS1@users.noreply.github.com>
2022-05-09 21:36:27 +0800

    Create CZRJXYTestr.kt

---
+++ src/main/java/test/CZRJXYTestr.kt
@@ -0,0 +1,13 @@
+package main.java.test
+
+import main.java.parser.GZRJXYParser
+
+
+
+fun main() {
+    //广州软件学院
+    val username=""
+    val password=""
+    val parser = GZRJXYParser(username,password)
+    parser.saveCourse()
+}
